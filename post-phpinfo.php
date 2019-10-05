2c7720fae0693b80a428978389e849002d7db228
Matthias Pigulla <mp@webfactory.de>
2019-10-05 18:42:33 +0000

    Use a PHP script to post data

---
+++ post-phpinfo.php
@@ -0,0 +1,34 @@
+<?php
+
+ob_start();
+phpinfo();
+$info = ob_get_clean();
+
+$data = json_encode(
+    [
+        'public' => 'true',
+        'files'  => [
+            $argv[2] => ['content' => $info],
+        ],
+    ]
+);
+
+$url = 'https://api.github.com/gists/' . $argv[1];
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+curl_setopt(
+    $ch,
+    CURLOPT_HTTPHEADER,
+    [
+        'Content-Type: application/javascript',
+        'Authorization: token ' . trim(file_get_contents(getenv('HOME') . '/.gist')),
+        'User-Agent: php/curl',
+    ]
+);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+curl_close($ch);
+
+print_r($result);
